166e6888bfcd0fcbd9be11f73ba68b6f6de83cc9
Mahshooq Zubair <kmahshooq@gmail.com>
2024-01-21 05:52:12 +0530

    Add product API endpoints to the tenant blueprint

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,10 +3,12 @@
 from flask import Blueprint
 
 public_bp = Blueprint("public", __name__, url_prefix="/api/v1/")
+tenant_bp = Blueprint("tenant", __name__, url_prefix="/api/v1/")
+
 # pylint: disable=wrong-import-position
 from . import (
     views,
     auth,
     shop,
-
+    product,
 )
---
+++ src/api/product.py
@@ -0,0 +1,57 @@
+"""Products APIs"""
+
+from flask import request
+from flask.views import MethodView
+
+
+from src.extensions import db
+from src.models.tenant import Product
+from src.serializers.tenant import ProductSchema
+from src.api import tenant_bp
+
+
+class ProductsAPI(MethodView):
+    """Products API"""
+
+    def get(self):
+        """Get products"""
+        products = Product.query.all()
+        return ProductSchema(many=True).dump(products)
+
+    def post(self):
+        """Create a new product"""
+        product = ProductSchema().load(request.json)
+        db.session.add(product)
+        db.session.commit()
+        return ProductSchema().dump(product), 201
+
+
+class ProductAPI(MethodView):
+    """Product API"""
+
+    def get(self, product_id):
+        """Get a product"""
+        product = Product.query.get_or_404(product_id)
+        return ProductSchema().dump(product)
+
+    def put(self, product_id):
+        """Update a product"""
+        product = Product.query.get_or_404(product_id)
+        product = ProductSchema().load(request.json, instance=product)
+        db.session.commit()
+        return ProductSchema().dump(product)
+
+    def delete(self, product_id):
+        """Delete a product"""
+        product = Product.query.get_or_404(product_id)
+        db.session.delete(product)
+        db.session.commit()
+        return "", 204
+
+
+tenant_bp.add_url_rule(
+    "/products/", view_func=ProductsAPI.as_view("products_api")
+)
+tenant_bp.add_url_rule(
+    "/products/<int:product_id>/", view_func=ProductAPI.as_view("product_api")
+)
